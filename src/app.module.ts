19f7052333771bdf54977d61e6fe90eea83db5b1
“Alex <alex.gomes.net3@gmail.com>
2023-05-01 14:43:29 -0300

    Install and Config Mongoose part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://alexgomesnet3:M9T8eQgd63KNBd0J@cluster0.hfsjc9j.mongodb.net/smartranking',
+      {
+        userNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
