b27097577c0f058fec78011bf8f222a4f784ef3b
“Alex <alex.gomes.net3@gmail.com>
2023-05-01 14:57:33 -0300

    Config Mongoose New Schema Jogador

---
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+
+export const JogadorSchema = new mongoose.Schema(
+  {
+    telefoneCelular: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    nome: {
+      type: String,
+    },
+    ranking: {
+      type: String,
+    },
+    posicaoRanking: {
+      type: Number,
+    },
+    urlFotoJogador: {
+      type: String,
+    },
+  },
+  { timestamp: true, collection: 'jogadores' },
+);
