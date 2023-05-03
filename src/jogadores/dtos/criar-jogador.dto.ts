1411bac105c0f53d5731b8c0e6970d69f1ee161a
“Alex <alex.gomes.net3@gmail.com>
2023-05-02 22:41:18 -0300

    Insert Pipe in Jogador Dto

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,5 +1,10 @@
+import { IsEAN, IsNotEmpty } from 'class-validator';
+
 export class CriarJogadorDto {
+  @IsNotEmpty()
   readonly telefoneCelular: string;
+  @IsEAN()
   readonly email: string;
+  @IsNotEmpty()
   readonly nome: string;
 }
