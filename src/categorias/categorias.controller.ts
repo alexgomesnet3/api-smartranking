1a993957de8b6999eae3b6401dd4415a1c3949c4
“Alex <alex.gomes.net3@gmail.com>
2023-05-15 11:41:57 -0300

    Created Criar Categoria Dto

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+  constructor(private readonly categoriasService: CategoriasService) {}
+
+  async criarCategoria(
+    @Body(criarCategoriaDto: CriarCategoriaDto)
+  )
+}
