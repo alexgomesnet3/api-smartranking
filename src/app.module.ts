5c8d68f2c6c3bda48a2718d347de80682c092fd5
“Alex <alex.gomes.net3@gmail.com>
2023-05-15 09:43:00 -0300

    Created Module, Service and Controller - Categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoriasService } from './categorias/categorias.service';
+import { CategoriasModule } from './categorias/categorias.module';
+import { CategoriasController } from './categorias/categorias.controller';
+import { CategoriasService } from './categorias/categorias.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env' }),
     MongooseModule.forRoot(`${process.env.MONGODB_URL}`, {}),
     JogadoresModule,
+    CategoriasModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [CategoriasController],
+  providers: [CategoriasService],
 })
 export class AppModule {}
