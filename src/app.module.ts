1d190ae7b73cfe904c84c8a50cdc84e9f45fd530
“Alex <alex.gomes.net3@gmail.com>
2023-05-20 22:48:18 -0300

    Created Module, Controller and Service Desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CategoriasModule } from './categorias/categorias.module';
     MongooseModule.forRoot(`${process.env.MONGODB_URL}`, {}),
     JogadoresModule,
     CategoriasModule,
+    DesafiosModule,
   ],
   controllers: [],
   providers: [],
