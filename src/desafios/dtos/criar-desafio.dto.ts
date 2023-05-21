ebc592dc941955b6e4b306b8541414cb17c82a34
“Alex <alex.gomes.net3@gmail.com>
2023-05-21 12:45:54 -0300

    Created CriarDesafio Dto

---
+++ src/desafios/dtos/criar-desafio.dto.ts
@@ -0,0 +1,20 @@
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+} from 'class-validator';
+import { Jogador } from '../../jogadores/interfaces/jogador.interface';
+
+export class CriarFDesafioDto {
+  @IsNotEmpty()
+  @IsDateString()
+  dataHoraDesafio: Date;
+  @IsNotEmpty()
+  solicitante: string;
+  @IsArray()
+  @ArrayMinSize(2)
+  @ArrayMaxSize(2)
+  jogadores: Array<Jogador>;
+}
