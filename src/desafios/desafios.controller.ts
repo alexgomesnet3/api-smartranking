260e1fd893a75406fa3da4f335328bb0e72b3680
“Alex <alex.gomes.net3@gmail.com>
2023-05-22 17:17:47 -0300

    Created CriarDesafio Handle

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CriarCategoriaDto } from 'src/categorias/dtos/criar-categoria.dto';
+import { Desafio } from './interfaces/desafio.interface';
 
 @Controller('desafios')
-export class DesafiosController {}
+export class DesafiosController {
+  @Post()
+  async criarDesafio(criarDesafioDto: CriarCategoriaDto): Promise<Desafio> {
+    return await criarDesafio(criarDesafioDto);
+  }
+}
